e807b6045c2c48abc847a4c8c4517839a6500be2
Ben Simms <ben@bensimms.moe>
2019-09-20 13:27:29 +0100

    Push changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,3 +82,14 @@ set SPACEFISH_GIT_STATUS_UNMERGED  " unmerged"
 
 
 set SPACEFISH_HASKELL_COLOR "yellow"
+
+function start_tmux
+    if type tmux > /dev/null
+        #if not inside a tmux session, and if no session is started, start a new session
+        if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT
+            tmux -2 attach; or tmux -2 new-session
+        end
+    end
+end
+
+start_tmux
