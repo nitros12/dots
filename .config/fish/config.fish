9dd43ea85efe3ab0baa3a962dd5faeb5ecb8b751
Ben Simms <ben@bensimms.moe>
2019-09-22 15:21:54 +0100

    Support local tmux stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,9 +65,13 @@ function start_tmux
   if status --is-login
       set PPID (echo (ps --pid %self -o ppid --no-headers) | xargs)
       if ps --pid $PPID | grep ssh
-          tmux has-session -t remote; and tmux attach-session -t remote; or tmux new-session -s remote; and kill %self
+          tmux has-session -t remote; and tmux -2 attach-session -t remote; or tmux -2 new-session -s remote; and kill %self
           echo "tmux failed to start; using plain fish shell"
       end
+  else
+    if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT
+        tmux -2 attach-session; or exec tmux -2 new-session
+    end
   end
 end
 
